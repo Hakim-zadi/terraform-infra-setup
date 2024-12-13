13a27637d0b6ef1f1d66fc5e62a8e73de79240f6
Hakim-zadi <kinzanawaz585@gmail.com>
2024-12-13 04:54:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-west-1"
+  region = var.region
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami           = var.ami
+  instance_type = var.instance_type
 
   tags = {
     Name = "TerraformExampleVM"
